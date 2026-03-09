435ad649e2faeb37bd9a1c5d153dfa272bd020b8
sailokeshnelluri <nellurisailokesh23@gmail.com>
2026-03-09 10:47:05 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
-from encryption import aes_encrypt, aes_decrypt
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+
+# allow connections from any device/browser
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
 @app.route("/")
 def login():
@@ -13,18 +14,19 @@ def login():
 def chat():
     return render_template("chat.html")
 
+# message event
 @socketio.on("message")
 def handleMessage(msg):
+    print("Message:", msg)
 
-    # decrypt incoming encrypted message
-    decrypted = aes_decrypt(msg)
+    # broadcast message to all users except sender
+    send(msg, broadcast=True, include_self=False)
 
-    print("Decrypted:", decrypted)
+# typing indicator
+@socketio.on("typing")
+def typing():
+    emit("typing", broadcast=True, include_self=False)
 
-    # encrypt again before sending
-    encrypted = aes_encrypt(decrypted)
-
-    send(encrypted, broadcast=True, include_self=False)
-
+# run server
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=5000)
