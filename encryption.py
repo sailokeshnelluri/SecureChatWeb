bbd58eefee7ac0d40221ed85513c8cf8d5bc342a
sailokeshnelluri <nellurisailokesh23@gmail.com>
2026-03-09 10:04:05 +0530

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,14 +1,18 @@
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import os
+import rsa
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
-# 256-bit AES key
-key = os.urandom(32)
+# AES KEY
+AES_KEY = os.urandom(32)
 
-def encrypt(message):
+
+# AES Encrypt
+def aes_encrypt(message):
 
     iv = os.urandom(16)
 
-    cipher = Cipher(algorithms.AES(key), modes.CFB(iv))
+    cipher = Cipher(algorithms.AES(AES_KEY), modes.CFB(iv))
+
     encryptor = cipher.encryptor()
 
     ciphertext = encryptor.update(message.encode()) + encryptor.finalize()
@@ -16,12 +20,30 @@ def encrypt(message):
     return iv + ciphertext
 
 
-def decrypt(ciphertext):
+# AES Decrypt
+def aes_decrypt(ciphertext):
 
     iv = ciphertext[:16]
     actual_cipher = ciphertext[16:]
 
-    cipher = Cipher(algorithms.AES(key), modes.CFB(iv))
+    cipher = Cipher(algorithms.AES(AES_KEY), modes.CFB(iv))
+
     decryptor = cipher.decryptor()
 
     return decryptor.update(actual_cipher).decode()
+
+
+# RSA Keys
+(publicKey, privateKey) = rsa.newkeys(2048)
+
+
+# RSA Encrypt
+def rsa_encrypt(message):
+
+    return rsa.encrypt(message.encode(), publicKey)
+
+
+# RSA Decrypt
+def rsa_decrypt(cipher):
+
+    return rsa.decrypt(cipher, privateKey).decode()
